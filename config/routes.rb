8cb667b5b1a52007618c35c2a90de205afe55dc8
Jacob Heil <jheil@wooster.edu>
2014-08-07 15:15:06 -0400

    Added About and Credits pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # get 'pages/about'
+  get 'about', to: 'pages#about'
+
   resources :transcriptions
+  
+  root 'transcriptions#index'
+
+  get 'credits', to: 'pages#credits'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
